57ad730ceeefc9b0fdb8b446d2a47c36399b10b3
Jay Freeman (saurik) <saurik@saurik.com>
2008-06-26 01:57:06 +0000

    Intial VNC server for the iPhone.

---
+++ iphonevnc/LayerKit/LKPurpleServer.h
@@ -0,0 +1,19 @@
+#ifndef LAYERKIT_LKPURPLESERVER_H_
+#define LAYERKIT_LKPURPLESERVER_H_
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#import <CoreGraphics/CGImage.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+CGImageRef LKPurpleServerGetScreenImage(void *null);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif/*LAYERKIT_LKPURPLESERVER_H_*/
